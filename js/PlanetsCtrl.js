941fb0b72e782da9ab4e9e8000a488b8482000da
Alicia Poindexter <alcampbel@gmail.com>
2015-11-28 08:43:12 -0800

    add some styling

--- js/PlanetsCtrl.js
+++ js/PlanetsCtrl.js
@@ -1,10 +1,8 @@
 'use strict';
 
 app.controller('PlanetsCtrl', function($scope, $http) {
-  console.log("I'm not working");
   $http.get(`http://swapi.co/api/planets/?page=1&format=json`)
     .then(res => {
-      console.log(res);
       $scope.planets = res.data.results.map(planet => {
         planet.residents = planet.residents.map(resident => {
           var resident = { url: resident };
@@ -13,10 +11,8 @@ app.controller('PlanetsCtrl', function($scope, $http) {
         });
         return planet;
       });  
-      console.log($scope.planets);
     })
     .catch(error => {
-      console.error(error.status)
       $scope.error = true;
     });
 });
